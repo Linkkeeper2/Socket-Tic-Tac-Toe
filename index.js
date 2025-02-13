b65a966f82493dd7da02ee178a43ebbc9b07a071
Jacob Morello <alternationgd57@gmail.com>
2025-02-12 21:31:32 -0500

    You can create and join rooms

--- index.js
+++ index.js
@@ -11,14 +11,71 @@ const io = new Server(server);
 app.set('view engine', 'ejs')
 app.set('trust proxy', 1);
 
+class Game {
+    constructor(room) {
+        this.player1 = null;
+        this.player2 = null;
+        this.room = room;
+
+        this.board = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
+    }
+
+    static findGame(roomName) {
+        return games.find(game => game.room === roomName);
+    }
+}
+
+var games = [];
+
 io.on("connection", (socket) => {
     console.log(`${socket.id} has connected`);
 
     socket.on("create room", (name) => {
-        socket.join(name);
-        socket.currentRoom = name;
+        let game = Game.findGame(name);
 
-        io.to(name).emit("room created", name);
+        if (game) {
+            socket.emit("room exists", name);
+        }
+
+        else {
+            socket.join(name);
+            socket.currentRoom = name;
+
+            games.push(new Game(name));
+
+            game = Game.findGame(name);
+
+            game.player1 = socket;
+
+            io.to(name).emit("room created", name);
+        }
+    });
+
+    socket.on("join room", (name) => {
+        let game = Game.findGame(name);
+        
+        if (game) {
+            if (!game.player2) {
+                socket.join(name);
+                socket.currentRoom = name;
+
+                game.player2 = socket;
+
+                io.to(name).emit("user joined", name);
+            }
+            
+            else {
+                socket.emit("room full", name)
+            }
+        }
+
+        else {
+           socket.emit("no room", name);
+        }
     });
 });
 
